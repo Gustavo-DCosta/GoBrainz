a04495bcf5426cd160e0740b02fe35bb5bfc578f
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-05-24 21:56:41 +0200

    FIST COMMIT

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello and Welcome to GoBrainz, GoBrainz is a CLI tool like an assistant to help you manage your projects")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Gustavo-DCosta/GoBrainz
+
+go 1.24.2
