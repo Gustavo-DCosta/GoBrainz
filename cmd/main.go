032902f1ac9dadd5d2e64d48f84323ddc4684eb0
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-05-25 07:39:52 +0200

    ADD: syntax personalisation

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,58 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("Hello and Welcome to GoBrainz, GoBrainz is a CLI tool like an assistant to help you manage your projects")
+	var choice int8
+	color.Cyan("Welcome to GoBrainz, a CLI assistant tool")
+	
+	color.Magenta("Before starting, would you like to select the syntax that you are most comfortable with?")
+	color.Yellow(`
+	1. @argument
+	2. argument?
+	3. GoBrainz argument
+	`)
+
+	fmt.Scan(&choice)
+
+	for {
+  	switch(choice) {
+		case 1: 
+			color.Green("Very well let's start...")
+			file, err := os.Create("syntax.txt")
+
+			if err != nil {
+				fmt.Println("There was an error creating the cache file")
+			}
+
+			defer file.Close() 
+			break
+	
+			case 2:
+			color.Green("Very well let's start...")
+			file, err := os.Create("syntax.txt")
+
+			if err != nil {
+				fmt.Println("Error creating cache file")
+			}
+
+			defer file.Close()
+			break
+	case 3:
+			color.Green("Very well let's start...")
+			file, err := os.Create("syntax.txt")
+
+			if err != nil {
+				fmt.Println("Error creating cache file")
+			}
+			defer file.Close()
+			break
+	}
+
+	}
+
 }
