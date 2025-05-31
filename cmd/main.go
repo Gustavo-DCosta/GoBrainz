6e623b87b9f7e2b84316d5e2e0643bb66082f1bf
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-05-31 11:23:11 +0200

    REMAKE

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
-
-	services "github.com/Gustavo-DCosta/GoBrainz/internal/Services"
-	//"github.com/fatih/color"
+	"github.com/fatih/color"
 )
 
 func main() {
-	_, err := os.Stat("syntax.txt")
+	color.Cyan(`        Welcome to GoBrainz
+GoBrainz is a CLI tool to help manage Notion projects with simple commands`)
 
-	if errors.Is(err, os.ErrNotExist) {
-		fmt.Println("Interal function working")
-	}
-	var choice int8
-
-	services.PromptOut()
-
-	fmt.Scan(&choice)
-
-	switch choice {
-	case 1:
-		services.CreatingCacheFile(int(choice))
-		services.EngineStart()
-		break
-
-	case 2:
-		services.CreatingCacheFile(int(choice))
-		services.EngineStart()
-		break
-	case 3:
-		services.CreatingCacheFile(int(choice))
-		services.EngineStart()
-		break
-	}
 }
